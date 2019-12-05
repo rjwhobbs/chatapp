62e5c717d43f456006f10d78da9cb1e6cd69cb47
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-05 14:27:06 +0200

    Do you even tree

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.set('views', './views');
+
 app.use(express.static('public')); // registring middleware with express app
 									// when getting requests this middle ware will be invoked, 
 									// which helps serve up static content.
 app.use(express.static('node_modules/bootstrap/dist'))
 
-app.get('/hello', function(req, res) {
-	res.send('This is a sentence');
+app.get('/', function(req, res) {
+	res.render('index.pug');
+});
+app.get('/admin/rooms', function(req, res) {
+	res.render('rooms.pug');
 });
 
 app.listen(3000, function() {
