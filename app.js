25c21fe19875d55044731e13dc95bd0db2d1aef9
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-05 15:25:37 +0200

    You can use tabs or spaces but not both in pug

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+let rooms = require('./data/rooms.json');
 
 app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.use(express.static('public')); // registring middleware with express app
 									// when getting requests this middle ware will be invoked, 
@@ -9,10 +11,13 @@ app.use(express.static('public')); // registring middleware with express app
 app.use(express.static('node_modules/bootstrap/dist'))
 
 app.get('/', function(req, res) {
-	res.render('index.pug');
+	res.render('index', {title: 'Home'});
 });
 app.get('/admin/rooms', function(req, res) {
-	res.render('rooms.pug');
+	res.render('rooms', {
+		title: 'Admin rooms',
+		rooms: rooms
+	});
 });
 
 app.listen(3000, function() {
