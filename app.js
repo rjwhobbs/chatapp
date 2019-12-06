8084b4e8f5e492790f20a99e75f51432e9f7d8fb
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-06 10:34:08 +0200

    Do you even body parse

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 let rooms = require('./data/rooms.json');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.use(express.static('public')); // registring middleware with express app
-									// when getting requests this middle ware will be invoked, 
-									// which helps serve up static content.
-app.use(express.static('node_modules/bootstrap/dist'))
+// registring middleware with express app
+// when getting requests this middle ware will be invoked, 
+// which helps serve up static content.
+app.use(express.static('public'));
+// This is redundant as I'm linking directly to bootstrap.
+app.use(express.static('node_modules/bootstrap/dist'));
+// This needs to be registered before any routes that rely on it
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 	res.render('index', {title: 'Home'});
 });
+
 app.get('/admin/rooms', function(req, res) {
 	res.render('rooms', {
 		title: 'Admin rooms',
 		rooms: rooms
 	});
+});
+
+app.get('/admin/rooms/add', function(req, res) {
+	res.render('add');
+});
+
+app.post('/admin/rooms/add', function(req, res) {
+	debugger;
+	res.send("nothing");
 });
 
 app.listen(3000, function() {
