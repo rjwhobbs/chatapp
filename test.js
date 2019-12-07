464c18478737405318e08967e500e3f0e8b363c1
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-12-07 17:55:47 +0200

    Object reference test

--- test.js
+++ test.js
@@ -12,14 +12,23 @@ const _ = require('lodash');
 // console.log(room);
 // console.log(rooms);
 
-var objectTest = [
-	{name: 'D', id: 4},
-	{name: 'E', id: 5},
-	{name: 'F', id: 6},
-];
+// var objectTest = [
+// 	{name: 'D', id: 4},
+// 	{name: 'E', id: 5},
+// 	{name: 'F', id: 6},
+// ];
 
-var test = objectTest[1];
-test.name = "WHAT";
-console.log (objectTest[1].name); 
-objectTest[1].name = "THIS";
-console.log(test.name);
+// var test = objectTest[1];
+// test.name = "WHAT";
+// console.log (objectTest[1].name); 
+// objectTest[1].name = "THIS";
+// console.log(test.name);
+
+var objectTest = {
+	name: "a",
+	id: 1
+}
+
+var refObjTest = objectTest;
+refObjTest.name = 'b';
+console.log(objectTest.name);
