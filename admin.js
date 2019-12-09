d43b778dbdfac84aeb5c86529e534568bcbb1377
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-09 13:49:30 +0200

    Things

--- admin.js
+++ admin.js
@@ -30,23 +30,21 @@ router.route('/rooms/add')
 });
 
 router.route('/rooms/edit/:id')
-	.get(function(req, res) {
+	.all(function(req, res, next) {
 		var roomId = req.params.id;
 		var room = _.find(rooms, r => r.id === roomId);
 		if(!room) {
 			res.sendStatus(404);
 			return ;
 		}
-		res.render('edit', {room});
+		res.locals.room = room; // This set the var in the render? render used to have ('edit', {room})
+		next();
+	})
+	.get(function(req, res) {
+		res.render('edit');
 	})
 	.post(function(req, res) {
-		var roomId = req.params.id;
-		var room = _.find(rooms, r => r.id === roomId);
-		if(!room) {
-			res.sendStatus(404);
-			return ;
-		}
-		room.name = req.body.name;
+		res.locals.room.name = req.body.name;
 		res.redirect(req.baseUrl + '/rooms');
 });
 
