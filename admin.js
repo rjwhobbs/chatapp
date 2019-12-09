4015e12e7636168a1504d86b21c3ec2921fa2c89
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-09 09:05:26 +0200

    .route method

--- admin.js
+++ admin.js
@@ -8,7 +8,7 @@ var router = express.Router();
 module.exports = router;
 
 router.get('/rooms', function(req, res) {
-	var origUrl = req.originalUrl;
+	var origUrl = req.originalUrl; // The calling URL if you will , /admin/rooms
 	res.render('rooms', {
 		title: 'Admin rooms',
 		rooms: rooms,
@@ -16,38 +16,38 @@ router.get('/rooms', function(req, res) {
 	});
 });
 
-router.get('/rooms/add', function(req, res) {
-	res.render('add');
+router.route('/rooms/add')
+	.get(function(req, res) {
+		res.render('add');
+	})
+	.post(function(req, res) {
+		var room = {
+			name: req.body.name,
+			id: uuid()
+		}
+		rooms.push(room);
+		res.redirect(req.baseUrl + '/rooms');
 });
 
-router.post('/rooms/add', function(req, res) {
-	var room = {
-		name: req.body.name,
-		id: uuid()
-	}
-	rooms.push(room);
-	res.redirect(req.baseUrl + '/rooms');
-});
-
-router.get('/rooms/edit/:id', function(req, res) {
-	var roomId = req.params.id;
-	var room = _.find(rooms, r => r.id === roomId);
-	if(!room) {
-		res.sendStatus(404);
-		return ;
-	}
-	res.render('edit', {room});
-});
-
-router.post('/rooms/edit/:id', function(req, res) {
-	var roomId = req.params.id;
-	var room = _.find(rooms, r => r.id === roomId);
-	if(!room) {
-		res.sendStatus(404);
-		return ;
-	}
-	room.name = req.body.name;
-	res.redirect(req.baseUrl + '/rooms');
+router.route('/rooms/edit/:id')
+	.get(function(req, res) {
+		var roomId = req.params.id;
+		var room = _.find(rooms, r => r.id === roomId);
+		if(!room) {
+			res.sendStatus(404);
+			return ;
+		}
+		res.render('edit', {room});
+	})
+	.post(function(req, res) {
+		var roomId = req.params.id;
+		var room = _.find(rooms, r => r.id === roomId);
+		if(!room) {
+			res.sendStatus(404);
+			return ;
+		}
+		room.name = req.body.name;
+		res.redirect(req.baseUrl + '/rooms');
 });
 
 router.get('/rooms/delete/:id', function(req, res) {
