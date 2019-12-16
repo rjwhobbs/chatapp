8fee0eaf631b77482ac9e543c00cd4694cb5ab40
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-12-16 16:51:26 +0200

    Modules in nodejs are singletons, You can modify data as if it's by reference, I'm not exactly sure how this works though but this basic test kind of explains it. On top of that one can require modules without exporting them. In other words you can run the script just by requirng it. Back to singletons: They are only loaded once, maybe this has something to with them being able to be referenced.

---
+++ playground/test.js
@@ -0,0 +1,6 @@
+let data = require('./data');
+
+console.log(data);
+let test2 = require('./test2');
+console.log("back in test");
+console.log(data);
---
+++ playground/test2.js
@@ -0,0 +1,4 @@
+let data = require('./data');
+
+data.push({thing: 'thing'});
+console.log("Test 2 ran");
